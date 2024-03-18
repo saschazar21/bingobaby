21a73f6164fbc0b3893663717ac3de01a9871e8f
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2024-03-18 10:57:43 +0100

    feat: added favicon sets

--- app/root.tsx
+++ app/root.tsx
@@ -118,6 +118,28 @@ export default function App() {
           rel="stylesheet"
           href="https://esm.sh/gardevoir@1.0.0/dist/index.min.css"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00667a" />
+        <meta name="msapplication-TileColor" content={pkg.color} />
+        <meta name="theme-color" content={pkg.color} />
         <Meta />
         <Links />
       </head>

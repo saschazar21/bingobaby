8ec422bd89263496d4f2f8c4e0a8db44c6553dfb
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2024-03-19 16:04:50 +0100

    fix: fixed html lang= attribute

--- app/root.tsx
+++ app/root.tsx
@@ -101,7 +101,7 @@ export default function App() {
   );
 
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />

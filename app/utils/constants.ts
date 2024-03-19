17a8db4611fd042e1747e82431c045d0a7d68085
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2024-03-19 15:56:05 +0100

    fix: fixed unnecessary string cast

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,7 +3,7 @@ import { dateObject, lockDate } from "./day";
 export const BIRTHDATE = "birthdate";
 
 export const MAX_GUESSES = !isNaN(parseInt(process.env.MAX_GUESSES ?? "", 10))
-  ? parseInt(process.env.MAX_GUESSES as string, 10)
+  ? parseInt(process.env.MAX_GUESSES, 10)
   : 3;
 
 export const CALCULATED_BIRTHDATE = process.env.CALCULATED_BIRTHDATE;

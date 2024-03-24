a5fe0f0fb1d978035deaf5616d76d8634fe84038
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2024-03-24 12:05:53 +0100

    feat: added confetti

--- app/components/Results/ResultsModal/index.tsx
+++ app/components/Results/ResultsModal/index.tsx
@@ -6,11 +6,24 @@ import { useResultsModal } from "./useResultsModal";
 export interface ResultsModalProps {}
 
 export const ResultsModal: FC = () => {
-  const { ref } = useResultsModal();
+  const { canvas, isCanvasShown, ref } = useResultsModal();
 
   return (
     <Modal heading="So sehen Sieger aus!" ref={ref}>
       <ResultsTable />
+      {isCanvasShown && (
+        <canvas
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 2,
+          }}
+          ref={canvas}
+        />
+      )}
     </Modal>
   );
 };
--- app/components/Results/ResultsModal/useResultsModal.ts
+++ app/components/Results/ResultsModal/useResultsModal.ts
@@ -1,17 +1,72 @@
-import { useEffect, useRef } from "react";
+import { usePubSubContext } from "@/contexts/PubSubContext";
+import { DIALOG_ACTIONS } from "@/utils/pubsub";
+import confetti from "canvas-confetti";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const RESULTS_HAVE_BEEN_SHOWN = "results have been shown";
 
 export const useResultsModal = () => {
+  const canvas = useRef<HTMLCanvasElement>(null);
   const ref = useRef<HTMLDialogElement>(null);
+  const pubSub = usePubSubContext();
+  const [isCanvasShown, setIsCanvasShown] = useState(true);
+
+  const fireConfetti = useCallback(() => {
+    if (canvas.current) {
+      confetti.create(canvas.current, {
+        disableForReducedMotion: true,
+        useWorker: false,
+      })({
+        particleCount: 200,
+        origin: {
+          y: 1,
+        },
+      })?.then(() => setIsCanvasShown(false));
+    }
+  }, []);
+
+  const renderCanvas = useCallback((value?: string) => {
+    value === DIALOG_ACTIONS.OPEN && setIsCanvasShown(true);
+  }, []);
+
+  useEffect(() => {
+    const modal = ref.current;
+    const el = canvas.current;
+
+    const [height, width] = [modal?.clientHeight, modal?.clientWidth];
+
+    if (el && height && width) {
+      el.height = height;
+      el.width = width;
+    }
+  }, []);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (isCanvasShown) {
+      timeout = setTimeout(fireConfetti, 300);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [fireConfetti, isCanvasShown]);
 
   useEffect(() => {
     const hasBeenShown = localStorage.getItem(RESULTS_HAVE_BEEN_SHOWN);
-    if (hasBeenShown !== "true") {
+    if (hasBeenShown !== "false") {
+      setIsCanvasShown(true);
       ref.current?.showModal();
       localStorage.setItem(RESULTS_HAVE_BEEN_SHOWN, "true");
     }
   }, []);
 
-  return { ref };
+  useEffect(() => {
+    const unsubscribe = pubSub?.subscribe(renderCanvas);
+
+    return () => {
+      typeof unsubscribe === "function" && unsubscribe();
+    };
+  }, [pubSub, renderCanvas]);
+
+  return { canvas, isCanvasShown, ref };
 };
